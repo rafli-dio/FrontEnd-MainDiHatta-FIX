29aff858d1262f11d97d0fa03a22404f4ee276a6
rafli-dio <raflidiomuhammadvalent@gmail.com>
2026-01-31 18:34:56 +0700

    perbaikan konfigurasi library axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,9 +5,30 @@ const axiosInstance: AxiosInstance = axios.create({
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/json',
     },
     withCredentials: true, 
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const getCookie = (name: string) => {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) {
+            return decodeURIComponent(parts.pop()?.split(';').shift() || '');
+        }
+        return null;
+    };
+
+    const token = getCookie('XSRF-TOKEN');
+
+    if (token) {
+        config.headers['X-XSRF-TOKEN'] = token;
+    }
+
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default axiosInstance;
